70025133009b6e0305ab385cdd2e8ddcf86e4623
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 07:00:28 -0600

    Update bucin.js

--- plugins/bucin.js
+++ plugins/bucin.js
@@ -1,27 +1,20 @@
 let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`“${pickRandom(global.bucin)}”`, m)
+  conn.reply(m.chat,`“${pickRandom(global.Frases)}”`, m)
 }
-handler.help = ['bucin']
-handler.tags = ['quotes']
-handler.command = /^(bucin)$/i
+handler.help = ['Frases']
+handler.tags = ['Citas']
+handler.command = /^(Frases)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
-
 handler.admin = false
 handler.botAdmin = false
-
 handler.fail = null
-
 module.exports = handler
-
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
-
-// https://jalantikus.com/tips/kata-kata-bucin/
-global.bucin = [
-  "Mau"
-]
+global.bucin = 
+  [ "Mau " ]
